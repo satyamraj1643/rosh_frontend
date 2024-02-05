f37dae80e4469dd7bf54f22dc5a104f26bb0bbfd
satyamraj1643 <your_email@example.com>
2024-02-05 18:06:22 +0530

    Roshan frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { Route, Routes } from "react-router-dom";
+import Chatpage from "./Pages/Chatpage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/chats" element={<Chatpage />} exact />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import ChatProvider from "./Context/ChatProvider";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
